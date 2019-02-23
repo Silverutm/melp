5df6c8ed5bc85be7edfc6f8b3bce25a3acf24c55
Silverio <rb2k4e@gmail.com>
2019-02-23 09:16:27 -0600

    statistical

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ express()
   .post('/restaurants', db.createRestaurant)
   .put('/restaurants/:id', db.updateRestaurant)
   .delete('/restaurants/:id', db.deleteRestaurant)
+  .get('/restaurants/statistics', db.getStatistics )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 
 /*create table Restaurants(id TEXT PRIMARY KEY, rating INTEGER, name TEXT, site TEXT, email TEXT, phone TEXT, street TEXT, city TEXT, state TEXT, lat FLOAT, lng FLOAT);*/
 //insert into Restaurants values('851f799f-0852-439e-b9b2-df92c43e7672',1,'Barajas, Bahena and Kano','https://federico.com','Anita_Mata71@hotmail.com','534 814 204','82247 Mariano Entrada','Merida Alfredotown','Durango',19.4400570537131,-99.1270470974249);
--- querys.js
+++ querys.js
@@ -72,10 +72,68 @@ const deleteRestaurant = (request, response) => {
     })
 }
 
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+  
+function distanceInMBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+    var earthRadiusKm = 6371;
+
+    var dLat = degreesToRadians(lat2-lat1);
+    var dLon = degreesToRadians(lon2-lon1);
+
+    lat1 = degreesToRadians(lat1);
+    lat2 = degreesToRadians(lat2);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    return earthRadiusKm * c * 1000;
+}
+
+const getStatistics = (request, response) => {
+    const { latitude, longitude, radius } = request.body
+    pool.query('SELECT * FROM Restaurants', (error, results) => {
+        if (error) {
+            throw error
+        }
+        let ans = {
+            count:0,
+            avg:0,
+            std:0
+        }
+        let rating = 0;
+        for (restaurant in results.rows)
+        {
+            if ( distanceInMBetweenEarthCoordinates(restaurant.lat, restaurant.lng, latitude, longitude) <= radius )
+            {
+                ans.count = ans.count + 1;
+                rating = rating  + restaurant.rating;
+            }
+        }
+        ans.avg = rating / count;
+        for (restaurant in results.rows)
+        {
+            if ( distanceInMBetweenEarthCoordinates(restaurant.lat, restaurant.lng, latitude, longitude) <= radius )
+            {
+                ans.std = (ans.avg - restaurant.rating) * (ans.avg - restaurant.rating);
+            }
+        }
+        ans.std = Mat.sqrt(ans.std / count);
+
+        response.status(200).json(ans)
+    })
+}
+
+
+
+  
+
 module.exports = {
     getRestaurants,
     getRestaurantById,
     createRestaurant,
     updateRestaurant,
     deleteRestaurant,
+    getStatistics,
 }
